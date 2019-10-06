8256eca1d7680d650584d05d1a00ce076fbb8081
konosh93 <omer.rayah@gmail.com>
2019-10-06 12:06:26 +0900

    Add code

---
+++ tests/graphql-codegen-documents.spec.ts
@@ -0,0 +1,10 @@
+import { validateTs } from '@graphql-codegen/testing';
+import { Types } from '@graphql-codegen/plugin-helpers';
+import { buildSchema, parse } from 'graphql';
+import { plugin } from '../src/index';
+
+describe('graphql-codegen-documents', () => {
+  it('should pass', async () => {
+    expect(1);
+  });
+})
